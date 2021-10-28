427b870eb923d9eb9c0fc77ae2f1d44f49e6651e
TongWY <2463509050@qq.com>
2021-10-28 19:19:36 +0800

    complete AWS stroe

--- app/src/main/java/com/example/mason/perquestron/Activity_perquestion.java
+++ app/src/main/java/com/example/mason/perquestron/Activity_perquestion.java
@@ -19,16 +19,12 @@ import com.example.mason.R;
 
 public class Activity_perquestion extends AppCompatActivity {
     private Button submit;
-    private RadioGroup radioGroup1;
-    private RadioGroup radioGroup2;
-    private RadioGroup radioGroup3;
-    private RadioGroup radioGroup4;
-    private RadioGroup radioGroup5;
-    private RadioButton radioButton1;
-    private RadioButton radioButton2;
-    private RadioButton radioButton3;
-    private RadioButton radioButton4;
-    private RadioButton radioButton5;
+
+    private String gender = null;
+    private String race = null;
+    private String ethnicity = null;
+    private String ses = null;
+    private String eigenstates = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +33,52 @@ public class Activity_perquestion extends AppCompatActivity {
 
         submit = findViewById(R.id.pre_submit);
 
-        radioGroup1 = findViewById(R.id.gender_group);
-        radioGroup2 = findViewById(R.id.race_group);
-        radioGroup3 = findViewById(R.id.eth_group);
-        radioGroup4 = findViewById(R.id.ses_group);
-        radioGroup5 = findViewById(R.id.emp_group);
+        RadioGroup radioGroup1 = (RadioGroup) findViewById(R.id.gender_group);
+        RadioGroup radioGroup2 = (RadioGroup) findViewById(R.id.race_group);
+        RadioGroup radioGroup3 = (RadioGroup) findViewById(R.id.eth_group);
+        RadioGroup radioGroup4 = (RadioGroup) findViewById(R.id.ses_group);
+        RadioGroup radioGroup5 = (RadioGroup) findViewById(R.id.emp_group);
 
-        radioButton1 = findViewById(radioGroup1.getCheckedRadioButtonId());
-        radioButton2 = findViewById(radioGroup2.getCheckedRadioButtonId());
-        radioButton3 = findViewById(radioGroup3.getCheckedRadioButtonId());
-        radioButton4 = findViewById(radioGroup4.getCheckedRadioButtonId());
-        radioButton5 = findViewById(radioGroup5.getCheckedRadioButtonId());
+        radioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                gender = radioButton.getText().toString();
+            }
+        });
+
+        radioGroup2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                race = radioButton.getText().toString();
+            }
+        });
+
+        radioGroup3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                ethnicity = radioButton.getText().toString();
+            }
+        });
+
+        radioGroup4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                ses = radioButton.getText().toString();
+            }
+        });
+
+        radioGroup5.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                eigenstates = radioButton.getText().toString();
+            }
+        });
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,13 +91,14 @@ public class Activity_perquestion extends AppCompatActivity {
                         success -> Log.i("MyAmplifyApp", "Saved a User."),
                         failure -> Log.e("MyAmplifyApp", "Save failed.", failure)
                 );
+
                 Amplify.DataStore.save(
                         Perception.builder()
-                                .gender(radioButton1.getText().toString())
-                                .race(radioButton2.getText().toString())
-                                .ethnicity(radioButton3.getText().toString())
-                                .ses(radioButton4.getText().toString())
-                                .ethnicity(radioButton5.getText().toString())
+                                .gender(gender)
+                                .race(race)
+                                .ethnicity(ethnicity)
+                                .ses(ses)
+                                .eigenstates(eigenstates)
                                 .build(),
                         result -> Log.i("MyAmplifyApp", "Created a new post successfully"),
                         error -> Log.e("MyAmplifyApp",  "Error creating post", error)
